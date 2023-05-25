0be4614f8428654c8f7788c64fa4a79e41f0cacc
Tyler Mueller <muelltyl@gmail.com>
2023-05-25 00:52:55 -0500

    fixes Tailwind plugin

--- main.ts
+++ main.ts
@@ -7,4 +7,7 @@
 import { start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
 
-await start(manifest, { port: 8080 });
+import twindPlugin from "$fresh/plugins/twind.ts";
+import twindConfig from "./twind.config.ts";
+
+await start(manifest, { plugins: [twindPlugin(twindConfig)], port: 8080 });
--- routes/index.tsx
+++ routes/index.tsx
@@ -10,6 +10,7 @@ export default function Home() {
         <title>Fresh App</title>
       </Head>
       <body class="flex ml-64 bg-gray-600">
+        <h1 class="text-black">Hello World!</h1>
         <div class="flex w-full flex-col overflow-auto">
         <Post username="chnoma" body="Hello!" title="Title"/>
         </div>
